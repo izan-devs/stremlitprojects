f912c1f88a4c0a4fdb32daed9fca42cba8baf822
izan-devs <izan3226@gmail.com>
2025-08-12 16:08:13 -0400

    Update parts.py

--- parts.py
+++ parts.py
@@ -49,7 +49,7 @@ if st.session_state.page == "home":
     with columns[0]:
         st.subheader("Suspension")
         st.image(suspension_url, caption="Suspension")
-        st.button("View more?")
+        st.button("View more?",key="one")
             
             
             
@@ -224,6 +224,7 @@ elif st.session_state.page == "contact":
                     
                 st.success("Info submitted successfully!")
                     
+
 
 
 
