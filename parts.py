31c1adc6247c6eb12c7fba89f9019248cefd4c5a
izan-devs <izan3226@gmail.com>
2025-08-12 14:55:51 -0400

    Update parts.py

--- parts.py
+++ parts.py
@@ -105,7 +105,7 @@ if st.session_state.page == "home":
             
             
 
-        submitted = st.form_submit_button("Submit!")
+        submitted = st.button("Submit!")
         if submitted:
             go_contact()
           
@@ -182,6 +182,7 @@ elif st.session_state.page == "contact":
                     
                 st.success("Info submitted successfully!")
                     
+
 
 
 
