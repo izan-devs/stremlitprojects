38820b90c03d7765d68efbf901e94727fddb1527
izan-devs <izan3226@gmail.com>
2025-08-13 14:38:12 -0400

    Update new.py

--- new.py
+++ new.py
@@ -45,7 +45,7 @@ oil_filter_url = "https://thumbs.dreamstime.com/b/oil-filters-20508237.jpg"
 st.button("Home",on_click=go_home)
 
 if st.session_state.page == "home":
-    st.image("MY LOGO.jpg",width=50)
+    st.image("MY LOGO.jpg",width=150)
     st.image(image,width=1250)
     
 
