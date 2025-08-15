991790fa48843ad95a02e28597fbfc003b735070
izan-devs <izan3226@gmail.com>
2025-08-14 22:22:02 -0400

    Update new.py

--- new.py
+++ new.py
@@ -118,7 +118,7 @@ st.button("Home",on_click=go_home)
 
 if st.session_state.page == "home":
     columns= st.columns([1,3,1])
-    with colcumns[1]:
+    with columns[1]:
         st.image("MY LOGO2.jpg",width=1000)
     st.image(image,width=1250)
     columns = st.columns(3)
